6dc7e88fd85113989b7d7c2697ccd3ff83d2c851
sawich <sawich@angeldev.org>
2021-01-21 15:03:00 +0200

    base channel

---
+++ SoulCore.IO.Network/BaseChannel.cs
@@ -0,0 +1,77 @@
+﻿using SoulCore.IO.Network.Commands;
+using SoulCore.IO.Network.Responses;
+using SoulCore.IO.Network.Utils;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoulCore.IO.Network
+{
+    public abstract class BaseChannel<TSession>
+        where TSession : SessionBase
+    {
+        public ushort Id { get; }
+
+        public IReadOnlyDictionary<Guid, TSession> Sessions => _internalSessions;
+
+        private readonly ConcurrentDictionary<Guid, TSession> _internalSessions = new();
+
+        protected BaseChannel(ushort id) =>
+            Id = id;
+
+        protected bool TryAdd(TSession session) =>
+            _internalSessions.TryAdd(session.Id, session);
+
+        protected bool TryRemove(TSession session, out TSession? @out) =>
+            _internalSessions.TryRemove(session.Id, out @out);
+
+        #region Broadcast World
+
+        protected void BroadcastDeferred(TSession session, SChannelBroadcastCharacterInResponse value) =>
+            BroadcastExceptDeferred(CategoryCommand.World, WorldCommand.InInfoPc, session, (writer) =>
+            {
+                writer.WriteCharacter(value.Character);
+                writer.WritePlace(value.Place);
+            });
+
+        protected void BroadcastDeferred(TSession session, SChannelBroadcastCharacterOutResponse value) =>
+            BroadcastExceptDeferred(CategoryCommand.World, WorldCommand.OutInfoPc, session, (writer) =>
+            {
+                writer.Write((byte)1); // count
+                writer.Write(value.Id);
+            });
+
+        #endregion Broadcast World
+
+        public void BroadcastDeferred(CategoryCommand category, object command, Action<PacketWriter> func)
+        {
+            using PacketWriter writer = new(category, command);
+            func(writer);
+
+            BroadcastDeferred(_internalSessions, writer);
+        }
+
+        public void BroadcastExceptDeferred(CategoryCommand category, object command, TSession except, Action<PacketWriter> func)
+        {
+            using PacketWriter writer = new(category, command);
+            func(writer);
+
+            BroadcastDeferred(_internalSessions.Where(pair => except.Id != pair.Key), writer);
+        }
+
+        private static void BroadcastDeferred(IEnumerable<KeyValuePair<Guid, TSession>> sessions, PacketWriter writer)
+        {
+            byte[] packet = GetRawPacket(writer);
+            foreach ((Guid _, TSession session) in sessions)
+                SendDeferred(session, packet, writer);
+        }
+
+        private static void SendDeferred(TSession session, byte[] packet, PacketWriter writer) =>
+            session.SendAsync(packet, 0, writer.BaseStream.Length);
+
+        private static byte[] GetRawPacket(PacketWriter writer) => PacketUtils.Pack(writer);
+    }
+}
+
+// https://youtu.be/l74Ot_2kuNs
---
+++ SoulCore.IO.Network/BaseChannelMember.cs
@@ -0,0 +1,172 @@
+﻿using SoulCore.IO.Network.Commands;
+using SoulCore.IO.Network.Requests;
+using SoulCore.IO.Network.Responses;
+using System.Linq;
+
+namespace SoulCore.IO.Network
+{
+    public abstract class BaseChannelMember<TChannel, TSession>
+        where TChannel : BaseChannel<TSession>
+        where TSession : SessionBase
+    {
+        protected readonly TChannel Channel;
+        protected readonly TSession Session;
+
+        #region Send Character Packet
+
+        public void SendDeferred(CharacterSpecialOptionListUpdateResponse value) =>
+            Session.SendDeferred(CategoryCommand.Character, CharacterCommand.UpdateSpecialOptionList, (PacketWriter writer) =>
+            {
+                writer.Write(value.Character);
+                writer.Write((byte)value.Values.Count());
+
+                foreach (CharacterSpecialOptionListUpdateResponse.Entity option in value.Values)
+                {
+                    writer.WriteSpecialOption(option.Id);
+                    writer.Write(option.Value);
+                }
+            });
+
+        #endregion Send Character Packet
+
+        #region Broadcast Chat Packet
+
+        public void BroadcastDeferred(ChatMessageResponse value) =>
+            Channel.BroadcastDeferred(CategoryCommand.Chat, ChatCommand.Normal, (writer) =>
+            {
+                writer.Write(value.Character);
+                writer.WriteChatType(value.Chat);
+                writer.WriteByteLengthUnicodeString(value.Message);
+            });
+
+        #endregion Broadcast Chat Packet
+
+        #region Broadcast World Packet
+
+        public void SendDeferred(CharacterOthersResponse value) =>
+            Session.SendDeferred(CategoryCommand.World, WorldCommand.OtherInfosPc, (PacketWriter writer) =>
+            {
+                writer.Write((short)value.Values.Count());
+                foreach (CharacterOthersResponse.Entity entity in value.Values)
+                {
+                    writer.WriteCharacter(entity.Character);
+                    writer.WritePlace(entity.Place);
+                }
+            });
+
+        #endregion Broadcast World Packet
+
+        #region Broadcast Character Packet
+
+        public void BroadcastDeferred(CharacterLevelSet value) =>
+            Channel.BroadcastDeferred(CategoryCommand.Character, CharacterCommand.Level, (writer) =>
+            {
+                writer.Write(value.Character);
+                writer.Write(value.Level);
+            });
+
+        #endregion Broadcast Character Packet
+
+        #region Broadcast Gesture Packet
+
+        public void BroadcastDeferred(CharacterGestureDo value) =>
+            Channel.BroadcastDeferred(CategoryCommand.Gesture, GestureCommand.Show, (writer) =>
+            {
+                writer.Write(value.Character);
+                writer.Write(value.Gesture);
+                writer.Write(value.Position);
+                writer.Write(uint.MinValue);
+                writer.Write(value.Rotation);
+            });
+
+        #endregion Broadcast Gesture Packet
+
+        // #region Broadcast Storage Packet
+
+        //public void BroadcastItemUpgradeResponse(BaseItem item)
+        //{ }
+
+        //public void BroadcastItemMove(params BaseItem[] slots)
+        //{
+        //    // var size
+        //    //     = sizeof(uint) /* Count */
+        //    //     + (SystemDefinition.ItemSize * slots.Length) /* Items */
+        //    //     + sizeof(byte) /* Unknown */;
+        //    //
+        //    // using PacketWriter writer = new(size, ClientOpcode.StorageItemMoveBroadcast);
+        //    //
+        //    // writer.Write(slots.Length);
+        //    // foreach (var item in slots)
+        //    // {
+        //    //     writer.Write(item);
+        //    // }
+        //    //
+        //    // writer.Write((byte)0);
+        //    //
+        //    // SendExceptAsync(session, writer);
+        //}
+
+        // #endregion Broadcast Storage Packet
+
+        #region Broadcast Movement Packet
+
+        public void BroadcastDeferred(CharacterToggleWeaponRequest request) =>
+            Channel.BroadcastDeferred(CategoryCommand.Move, MoveCommand.BattleBt, (writer) =>
+            {
+                writer.Write(request.Character);
+                writer.Write(request.Position);
+                writer.Write(request.Rotation);
+                writer.Write(request.Toggle);
+                writer.Write(request.Unknown1);
+            });
+
+        public void BroadcastDeferred(SRMMoveRequest request) =>
+            Channel.BroadcastDeferred(CategoryCommand.Move, MoveCommand.MoveBt, (writer) =>
+            {
+                writer.Write(request.Character);
+                writer.Write(request.Unknown1);
+                writer.Write(request.Position);
+                writer.Write(request.Rotation);
+                writer.Write(request.InterpolatedPosition);
+                writer.Write(request.Unknown5);
+                writer.Write(request.Unknown6);
+                writer.Write(request.Unknown7);
+                writer.Write(request.Unknown8);
+            });
+
+        public void BroadcastDeferred(SRMStopRequest request) =>
+            Channel.BroadcastDeferred(CategoryCommand.Move, MoveCommand.StopBt, (writer) =>
+            {
+                writer.Write(request.Character);
+                writer.Write(request.Unknown1);
+                writer.Write(request.Position);
+                writer.Write(request.Rotation);
+                writer.Write(request.Unknown4);
+                writer.Write(request.Unknown5);
+            });
+
+        public void BroadcastDeferred(SRMJumpRequest request) =>
+            Channel.BroadcastDeferred(CategoryCommand.Move, MoveCommand.JumpBt, (writer) =>
+            {
+                writer.Write(request.Character);
+                writer.Write(request.Unknown1);
+                writer.Write(request.Unknown2);
+                writer.Write(request.Location);
+                writer.Write(request.Unknown3);
+                writer.Write(request.Position);
+                writer.Write(request.Rotation);
+                writer.Write(request.InterpolatedPosition);
+                writer.Write(request.Unknown5);
+                writer.Write(request.Unknown6);
+            });
+
+        public void BroadcastDeferred(CharacterLoopMotionEndResponse value) =>
+            Channel.BroadcastDeferred(CategoryCommand.Move, MoveCommand.LoopMotionEndBt, (writer) =>
+                writer.Write(value.Character));
+
+        #endregion Broadcast Movement Packet
+
+        protected BaseChannelMember(TSession session, TChannel channel) =>
+            (Session, Channel) = (session, channel);
+    }
+}
