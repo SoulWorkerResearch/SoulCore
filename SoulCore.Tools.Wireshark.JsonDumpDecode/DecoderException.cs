d35a893a4bcea2c3ac1074374664462a0b665a63
sawich <sawich@angeldev.org>
2021-01-26 18:36:25 +0200

    move "SoulCore.Tools.Wireshark.JsonDumpDecode" project from OpenWorker

---
+++ SoulCore.Tools.Wireshark.JsonDumpDecode/DecoderException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SoulCore.Tools.Wireshark.JsonDumpDecode
+{
+    [Serializable]
+    public sealed class DecoderException : Exception
+    {
+        public DecoderException()
+        {
+        }
+
+        public DecoderException(string message) : base(message)
+        {
+        }
+
+        public DecoderException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ SoulCore.Tools.Wireshark.JsonDumpDecode/PacketDecoder.Packet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SoulCore.Tools.Wireshark.JsonDumpDecode
+{
+    public sealed partial class PacketDecoder
+    {
+        public sealed record Packet
+        {
+            public ulong Frame { get; init; }
+            public string Time { get; init; } = string.Empty;
+            public string Sender { get; init; } = string.Empty;
+            public string Receiver { get; init; } = string.Empty;
+            public byte[] Body { get; init; } = Array.Empty<byte>();
+        }
+    }
+}
