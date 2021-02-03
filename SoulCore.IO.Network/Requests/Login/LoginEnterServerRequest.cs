be26d8d16a402e2dfdceb1078d175d0f9e831df8
sawich <sawich@angeldev.org>
2021-02-03 13:53:16 +0200

    requests

---
+++ SoulCore.IO.Network/Requests/Login/LoginEnterServerRequest.cs
@@ -0,0 +1,22 @@
+﻿using SoulCore.Extensions;
+using SoulCore.Types;
+using System.IO;
+
+namespace SoulCore.IO.Network.Requests
+{
+    public readonly struct LoginEnterServerRequest
+    {
+        public readonly int AccountId;
+        public readonly ushort GateId;
+        public readonly ulong SessionKey;
+        public readonly EnterGateWay Way;
+
+        public LoginEnterServerRequest(BinaryReader br)
+        {
+            AccountId = br.ReadInt32();
+            GateId = br.ReadUInt16();
+            SessionKey = br.ReadUInt64();
+            Way = br.ReadEnterGateWayType();
+        }
+    }
+}
