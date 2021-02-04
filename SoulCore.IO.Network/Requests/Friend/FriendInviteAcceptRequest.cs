9b74b35563f6715d8aa3a31e2c5f760a23c2894c
sawich <sawich@angeldev.org>
2021-02-04 20:27:00 +0200

    requests

--- SoulCore.IO.Network/Requests/Friend/FriendInviteAcceptRequest.cs
+++ SoulCore.IO.Network/Requests/Friend/FriendInviteAcceptRequest.cs
@@ -6,9 +6,9 @@ using System.IO;
 namespace SoulCore.IO.Network.Requests.Friend
 {
     [Request(CategoryCommand.Friend, FriendCommand.InviteAccept)]
-    public readonly struct FriendAcceptRequest
+    public readonly struct FriendInviteAcceptRequest
     {
-        public FriendAcceptRequest(BinaryReader br)
+        public FriendInviteAcceptRequest(BinaryReader br)
         {
             throw new NotImplementedException();
         }
