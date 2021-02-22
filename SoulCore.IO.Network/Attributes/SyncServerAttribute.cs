bf877c651e8a0176f2d1033820b776b3483ad102
sawich <sawich@angeldev.org>
2021-02-22 03:18:52 +0200

    AddNetwork

--- SoulCore.IO.Network/Attributes/SyncServerAttribute.cs
+++ SoulCore.IO.Network/Attributes/SyncServerAttribute.cs
@@ -6,7 +6,7 @@ namespace SoulCore.IO.Network.Attributes
     [AttributeUsage(AttributeTargets.Class)]
     public sealed class SyncServerAttribute : Attribute
     {
-        public readonly SyncServerType Type;
+        internal readonly SyncServerType Type;
 
         public SyncServerAttribute(SyncServerType type = SyncServerType.None) => Type = type;
     }
--- SoulCore.IO.Network/Extensions/ServiceCollectionExtension.cs
+++ SoulCore.IO.Network/Extensions/ServiceCollectionExtension.cs
@@ -9,19 +9,29 @@ namespace SoulCore.IO.Network.Extensions
 {
     public static class ServiceCollectionExtension
     {
-        public static IServiceCollection AddNetwork<TServer, TSession>(this IServiceCollection services)
-        where TServer : BaseServer<TServer, TSession>
-        where TSession : BaseSession<TServer, TSession>
+        public static IServiceCollection AddNetwork(this IServiceCollection services)
         {
             foreach (Type type in GetSyncHandlers())
             {
                 services.AddTransient(type);
             }
 
-            return services
-                .AddSingleton<TServer>()
-                .AddTransient<TSession>();
+            foreach (Type type in GetByAttribute<SyncSessionAttribute>())
+            {
+                services.AddTransient(type);
+            }
+
+            foreach (Type type in GetByAttribute<SyncServerAttribute>())
+            {
+                services.AddSingleton(type);
+            }
+
+            return services;
         }
+
+        private static IEnumerable<Type> GetByAttribute<T>() where T : Attribute => AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => type.IsDefined(typeof(T)));
 
         private static IEnumerable<Type> GetSyncHandlers() => AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(assembly => assembly.GetTypes())
