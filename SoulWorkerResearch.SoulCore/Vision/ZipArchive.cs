e4a24ba5a6cf1ea6bb7867a32b4df56b6110049a
sawich <sawich@angeldev.org>
2021-04-19 22:19:10 +0300

    add ZipArchive class

---
+++ SoulWorkerResearch.SoulCore/Vision/ZipArchive.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace SoulWorkerResearch.SoulCore.Vision
+{
+    public class ZipArchive : ZipFile
+    {
+        protected ZipArchive(string path, string? password = null, byte xor = 0x55) : base(GetFile(path, xor), false)
+        {
+            Password = password;
+        }
+
+        private static Stream GetFile(string path, byte xor)
+        {
+            byte[] data = File.ReadAllBytes(path);
+            
+            for (var q = 0; q != data.Length; ++q)
+            {
+                data[q] ^= xor;
+            }
+
+            return new MemoryStream(data);
+        }
+    }
+}
