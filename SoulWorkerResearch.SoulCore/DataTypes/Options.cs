700eb0c8b6e37f84931804ee780b0152f2587f0b
sawich <sawich@angeldev.org>
2022-08-12 20:13:00 +0300

    add options type

---
+++ SoulWorkerResearch.SoulCore/DataTypes/Options.cs
@@ -0,0 +1,27 @@
+﻿using SoulWorkerResearch.SoulCore.Defines;
+
+namespace SoulWorkerResearch.SoulCore.DataTypes;
+
+public sealed class Options : List<bool>
+{
+    public static Options Empty { get; } = new();
+
+    public bool IsAttendanceEnabled => this[Option.Attendance];
+    public bool IsSecondPasswordEnabled => this[Option.SecondPassword];
+    public bool IsPvpDistrictEnabled => this[Option.PvpDistrict];
+    public bool IsRankingEnabled => this[Option.Ranking];
+    public bool IsCashShopEnabled => this[Option.CashShop];
+    public bool IsD6ModeEnabled => this[Option.D6Mode];
+    public bool IsBroachEventEnabled => this[Option.BroachEvent];
+    public bool IsOverIndulgenceEnabled => this[Option.OverIndulgence];
+    public bool IsSoulWeeklyMissionEnabled => this[Option.SoulWeeklyMission];
+    public bool IsNetCafeEnabled => this[Option.NetCafe];
+    public bool IsSoulEventEnabled => this[Option.SoulEvent];
+    public bool IsItemExchangeEnabled => this[Option.ItemExchange];
+    public bool IsWaitSystemEnabled => this[Option.WaitSystem];
+    public bool IsOperationMazeEnabled => this[Option.OperationMaze];
+
+    public bool this[Option index] => this[(int)index];
+
+    public Options() : base(Enumerable.Range(0, (byte)Option.Max).Select(_ => false)) { }
+}
